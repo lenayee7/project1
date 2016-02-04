e2780acec9402fe6578d9a052c327c3833d316c3
lenayee7 <Lena@Winston.local>
2016-02-04 14:51:15 -0800

    adds changes to js; game functions

--- js/app.js
+++ js/app.js
@@ -1,54 +1,90 @@
 $(document).ready(function() {
-
 	console.log('testing');
 	var count = 0;
+	var $cardTag1;
 	var color1;
+	var $cardTag2;
 	var color2;
+	var points=0;
+		//remove class "card" to reveal background-color on click
 
-	//remove class "card" to reveal background-color on click
+		// function playGame() {
+		// 	$('#playButton').one('click',function()
 
-	$('.card').on('click', function() {
-		$(this).removeClass('notFlipped');
-		$(this).addClass('flipped');
-		color1 = $(this).attr('data-color>');
-		console.log(color1);
-		console.log(this);
-		// count = count + 1;
-		// console.log(co lor1);
-		// color1 = $('.flipped').attr('data-color');
-		// color2 = $('.flipped').attr('data-color');
-		// check(color1,color2);
-	});
+			
 
-	//check if two boxes have been clicked
-	//if two boxes match - leave flipped
-	// function check(color1,color2) {
-	// 	if (count === 1) {
-	// 		if(color1 === color2){
-	// 			$(this).addClass('flipped');
-	// 			console.log(this);
-	// 		}
-	// 		count = 0;
-	// 		// console.log('notFlipped');
-	// 	}
-	// }
+	function getColorInitialize() {
 
-	// setTimeout(function(){
-	// 	$(".flipped").addClass('notFlipped');
-	// 	$(".notFlipped").removeClass('flipped');
-	// 	},200);
-	});
+		$('.card').click(function() {
+			console.log("Score:",points);
+			if( count === 0 ) {
+				//flip the card
+				$(this).removeClass('flipped');
+				$(this).attr('data-state', 'played')
+				//save the color into temp
+				$cardTag1=$(this);
+				color1 = $(this).attr("data-color");
+				count = 1;
+			} else if (count === 1 && $(this).attr("data-state")!=='played') {
+				$(this).removeClass('flipped');
+				$(this).attr("data-state",'played');
+				//save the color into temp 2
+				count = 2;
+				$cardTag2=$(this);
+				color2 = $(this).attr("data-color");
+				checkMatch();
+			}
+		});
+	}
+	//      if (color1 === color2) {
+			 // $(this).removeClass('flipped');
+		// 		console.log('Pair');
+		// 		count = 0;
+			// }
+		// } else {
+		// 	$(this).addClass('flipped');
+		// 	count = 0;
+			// $('.card').addClass('flipped')
 
-// });
+				//TODO: keep card open (unable to flip back)
+				// $(this).removeClass('flipped');
+
+	function checkMatch(){
+		console.log('Checking Match');
+		if(color1 === color2){
+			console.log("Got a match!", color1, color2)
+			// $(this).removeClass('flipped');
+			points++;
+			count = 0;
+		// return true
+		} else {
+			console.log("No match.. flip back")
+			// $(this).addClass('flipped');
+			count = 0;
+			flipBack($cardTag1,$cardTag2);
+			// setTimeout(function() {
+			// 	$('.card').addClass('flipped');
+			// }, 600);
+		// return false
+		}
+	}
+
+
+	function flipBack($tag1, $tag2) {
+		setTimeout(function(){
+			$tag1.addClass('flipped');
+			$tag2.addClass('flipped');
+			$tag1.attr('data-state',"");
+			$tag2.attr('data-state',"");
+		},500);
+	}
+
+		
+		// playGame();
+		getColorInitialize();
+});
 
 
 
-
-
-// function unflip ()
-	// $('.unflip').on('click', function() { 
-	// 	// $(this).removeClass('unflip');
-	// 	$(this).addClass('notFlipped');
-	// });
 
 
