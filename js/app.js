4dd029e4ca610e0015d19dd54a9abedb15db4be0
lenayee7 <Lena@Winston.local>
2016-02-05 09:25:21 -0800

    adds updated changes & shuffle

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 $(function() {
-
 	console.log('testing');
+	//created color array * 2
+	var colors = ["red","green","blue","orange","purple","brown","silver","teal","yellow"];
+	colors = colors.concat(colors);
+	//randomized colors
+	colors = shuffleArray(colors);
+	//created divs for each color & appended to #gameBoard
+	var boardHTML = '';
+	for (var i = 0; i < colors.length; i++) {
+		var str = '<div class="col-md-2 card flipped ' + colors[i] +'" data-color="' + colors[i] + '"></div>';
+		// console.log(colors[i]);
+		console.log(str);
+		boardHTML += str
+	}
+	$('#gameBoard').append(boardHTML);
+		// console.log(boardHTML);
+
 	var count = 0;
 	var $cardTag1;
 	var color1;
@@ -8,12 +23,20 @@ $(function() {
 	var color2;
 	var points = 1;
 	
+	function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    	}
+    	 return array;
+	}
 
 	//get color1 & color2 then checkMatch();
 	function getColorInit() {
 		$('.card').on('click', function() {
 			// console.log("Score:",points);
-
 			if( count === 0 ) {
 				//flip one card
 				$(this).removeClass('flipped');
@@ -39,8 +62,8 @@ $(function() {
 		console.log('Checking Match');
 		if(color1 === color2){
 			$('#score').html("You have " + points + " points");
-			console.log("Got a match!", color1, color2)
-			$cardTag1.off('click');
+			// console.log("Got a match!", color1, color2)
+			$cardTag2.off('click');
 			$cardTag2.off('click');
 			points ++;
 			// console.log('score: ' + points);
