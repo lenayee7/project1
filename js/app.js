17aad2bf6dd3ddd32f5d7e8c43fcf733e2036104
lenayee7 <Lena@Winston.local>
2016-02-06 20:52:59 -0800

    adds timer, changed background

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 $(function() {
+
+	var getName = prompt('What is your name?');
+			playerName = ('Yayy ' + getName + ' !!!' + ' Time to eat some M&M\'s!');
+
 	console.log('testing');
 	//created color array * 2
-	var colors = ["red","green","blue","orange","purple","brown","silver","teal","yellow"];
+	var colors = ["red","green","blue","orange","purple","brown","silver","teal","yellow","darkblue"];
 	colors = colors.concat(colors);
 	//randomized colors
 	colors = shuffleArray(colors);
 	//created divs for each color & appended to #gameBoard
 	var boardHTML = '';
 	for (var i = 0; i < colors.length; i++) {
-		var str = '<div class="col-md-2 col xs-1 card flipped ' + colors[i] +'" data-color="' + colors[i] + '"></div>';
+		var str = '<div class="card flipped ' + colors[i] +'" data-color="' + colors[i] + '"></div>';
 		// console.log(colors[i]);
 		// console.log(str);
 		boardHTML += str
 	}
+
 	$('#gameBoard').append(boardHTML);
 		// console.log(boardHTML);
-
 	var count = 0;
 	var $cardTag1;
 	var color1;
@@ -59,7 +63,7 @@ $(function() {
 	}
 
 	function checkMatch(){
-		console.log('Checking Match');
+		// console.log('Checking Match');
 		if(color1 === color2){
 			$('#score').html("You have " + points + " points");
 			// console.log("Got a match!", color1, color2)
@@ -68,8 +72,8 @@ $(function() {
 			points ++;
 			// console.log('score: ' + points);
 			count = 0;
-				if(points === 10) {
-					alert('Yayy! Click Reset button to play again');
+				if(points === 11) {
+				console.log(playerName);
 				}
 			// remove eventlisteners from matched cards					
 				
@@ -99,7 +103,27 @@ $(function() {
 		});	
 	}
 
-	getColorInit();
+	//start game 
+	function startGame() {
+		$('#play').one('click', function(timer){
+			$(this).toggleClass('click',timer);
+			getColorInit();
+			var count=60;
+			var counter = setInterval(timer, 1000);
+
+				function timer() {
+				  count = count - 1;
+				  console.log(count);
+				  $('#timer').text('Timer - ' + '0: ' + count);
+				  if (count <= 0) {
+				     clearInterval(counter);
+				     	console.log('game over')
+				     return;
+				  }
+				}
+		});
+	}
+	startGame();
 	resetGame();
 
 });
